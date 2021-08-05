3fa94c4d6367a17349c1162350d05733be32a4b6
Romel Benavides <romebenavides101@gmail.com>
2021-08-04 22:18:30 -0500

    push multiple changes of using aws cdk

---
+++ aws_cdk_projects/sso_build/lib/sso_build-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class SsoBuildStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+  }
+}
